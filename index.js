33a273b89272260553633e84851c8e468980e3ea
Bogdan3690 <dblevko@gmail.com>
2026-04-29 14:38:08 -0600

    start project Pokemon

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const BASEURL = "https://pokeapi.co/api/v2/"
+const form = document.querySelector(".js-search-form")
+console.log(form);
+const input = document.querySelector(".form-control")
+console.log(input);
+
+
+form.addEventListener("submit", onSearchBtn)
+
+function onSearchBtn(e) {
+    e.preventDefault()
+    const id = input.value
+    console.log(Number(id));
+    if (Number.isNaN(Number(id))) {
+        alert("Enter a Number")
+        return 
+    }
+}
+
+function fetchPokemonById(params) {
+    
+}
 
 fetch(`${BASEURL}pokemon/ditto`)
 .then(resp => {
